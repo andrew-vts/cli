4cd59530b2c8d8c8c8cbcbb67ae6b82b52a7cd30
Chad Carbert <chadcarbert@me.com>
2019-06-11 17:56:08 -0600

    Migrate webhooks:index command

--- packages/webhooks/src/commands/webhooks/index.ts
+++ packages/webhooks/src/commands/webhooks/index.ts
@@ -1,23 +1,48 @@
+import color from '@heroku-cli/color'
 import {Command, flags} from '@heroku-cli/command'
+import {cli} from 'cli-ux'
 
-export default class WebhooksIndex extends Command {
-  static description = 'describe the command here'
+import webhookType from '../../webhook-type'
+
+export default class Webhooks extends Command {
+  static description = 'list webhooks on an app'
+
+  static examples = ['$ heroku webhooks']
 
   static flags = {
     app: flags.app(),
     remote: flags.remote(),
-    pipeline: flags.string({ char: 'p', description: 'pipeline on which to list', hidden: true })
+    pipeline: flags.string({
+      char: 'p',
+      description:
+      'pipeline on which to list',
+      hidden: true
+    })
   }
 
   static args = [{name: 'file'}]
 
   async run() {
-    const {args, flags} = this.parse(WebhooksIndex)
+    const {flags} = this.parse(Webhooks)
+    let {path, display} = webhookType(flags)
 
-    const name = flags.name || 'world'
-    this.log(`hello ${name} from /Users/ccarbert/projects/heroku/cli/packages/webhooks/src/commands/webhooks/index.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
+    let {body} = await this.heroku.get(`${path}/webhooks`, {
+      headers: {Accept: 'application/vnd.heroku+json; version=3.webhooks'}
+    })
+    let webhooks = body
+
+    if (webhooks.length === 0) {
+      cli.log(`${display} has no webhooks\nUse ${color.cmd('heroku webhooks:add')} to add one.`)
+      return
     }
+
+    webhooks.sort((a: any, b: any) => Date.parse(a.created_at) - Date.parse(b.created_at))
+
+    cli.table(webhooks, {
+      id: {header: 'Webhook ID'},
+      url: {header: 'URL'},
+      include: {header: 'Include', get: (row: any) => row.include.join(',')},
+      level: {header: 'Level'}
+    })
   }
 }
